361e44106349fdb6baa9606ee5e2db5e3a77bcb0
Rafal Martinez <Archangel.raffael@gmail.com>
2015-11-25 05:23:37 +0100

    Admin module

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,7 @@ return array (
 		'modules' => array (
 				'ArchangelDB',
 				'Application',
-				'Display','Process', 
+				'Display','Process','Admin', 
 		)
 		,
 		
@@ -17,7 +17,7 @@ return array (
 				'module_paths' => array (
 						'./module',
 						'./vendor',
-						'./module','./module' 
+						'./module','./module','./module' 
 				),
 				
 				// An array of paths from which to glob configuration files after
