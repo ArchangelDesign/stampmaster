592cff347dc2b75f1f3eeed396ff2dff9b8e99e4
rafalm <archangel.raffael@gmail.com>
2015-12-09 14:02:24 +0100

    global application configuration

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Stamp Master
+ * General configuration file
+ * @author Rafal Martinez-Marjanski
+ * @copy Archangel Design <http://archangel-design.com>
+ */
+
+class SmConfig
+{
+    const domain    = 'sm.dev';
+    const http      = 'http://sm.dev';
+}
