4bbf0b178d9b0e8b95d3bac428e3fc33afb3f197
rafal-martinez <archangel.raffael@gmail.com>
2016-04-08 20:40:11 +0200

    new token generation method

--- module/Process/src/Common/Common.php
+++ module/Process/src/Common/Common.php
@@ -10,8 +10,27 @@ namespace Common;
 
 class Common
 {
+    /**
+     * @return string
+     */
     public static function generateToken()
     {
-        return md5(time());
+        return md5(self::generateRandomString());
+    }
+
+    public static function generateRandomString($length = 30) {
+        return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+    }
+
+    /**
+     * @param \ArchangelDB\ADB2 $db
+     * @param $table
+     * @param $key
+     */
+    public static function generateUniqueToken(\ArchangelDB\ADB2 $db, $table, $key)
+    {
+        $token = self::generateToken();
+        $check = $db->fetchOne($table, [], $key);
+        
     }
 }
